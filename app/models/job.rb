93728ec81ce67c2dd0b79df88277ab2ab8b9d8c0
[01;31m[K"name"[m[K: "Nick <nsksir@gmail.com>
2017-07-21 17:50:52 -0700

    test all associations tests check out and renamed view files to fit rails conventions

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,2 +1,4 @@
 class Job < ApplicationRecord
+  belongs_to :board
+  belongs_to :jobposting, class_name: 'User'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  has_many :subscriptions
+  has_many :boards, through: :subscriptions
+  has_many :jobpostings, class_name: 'Job',  foreign_key: "jobposting_id"
+  has_many :hostings, class_name: 'Board', foreign_key: "hosting_id"
+
+  def user_params
+    params.require(:user).permit(:username, :password, :email, :password_confirmation)
+  end
 end
